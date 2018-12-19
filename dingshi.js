eb0f021ea4abea5d431af525234d7feb83831273
yanghongxuan <782042369@qq.com>
2018-12-19 15:30:09 +0800

    node

--- dingshi.js
+++ dingshi.js
@@ -1,31 +1,47 @@
 // process.nextTick(function () {
-//   console.log('延迟执行');
-// });
-// console.log('正常执行');
+//   console.log('延迟执行')
+// })
+// console.log('正常执行')
 // setImmediate(function () {
-//   console.log('延迟执行');
-// });
-// console.log('正常执行');
+//   console.log('延迟执行')
+// })
+// console.log('正常执行')
 // process.nextTick(function () {
-//   console.log('nextTick延迟执行');
-// });
+//   console.log('nextTick延迟执行')
+// })
 // setImmediate(function () {
-//   console.log('setImmediate延迟执行');
-// });
-// console.log('正常执行');
+//   console.log('setImmediate延迟执行')
+// })
+// console.log('正常执行')
+// process.nextTick(function () {
+//   console.log(1)
+// })
+// process.nextTick(function () {
+//   console.log('nextTick延迟执行2')
+// }) // 加入两个setImmediate()的回调函数
+// setImmediate(function () {
+//   console.log('setImmediate延迟执行1') // 进入下次循环 
+//   process.nextTick(function () {
+//     console.log('插入')
+//   })
+// })
+// setImmediate(function () {
+//   console.log('setImmediate延迟执行1') // 进入下次循环 
+// })
 process.nextTick(function () {
-  console.log('nextTick延迟执行1');
-});
-process.nextTick(function () {
-  console.log('nextTick延迟执行2');
-}); // 加入两个setImmediate()的回调函数
+  console.log(1)
+})
+setTimeout(() => {
+  console.log(2)
+}, 0)
 setImmediate(function () {
-  console.log('setImmediate延迟执行1'); // 进入下次循环 
-  process.nextTick(function () {
-    console.log('插入');
-  });
-});
-setImmediate(function () {
-  console.log('setImmediate延迟执行2');
-});
-console.log('正常执行');
+  console.log(3)
+})
+let p = new Promise(function (resolve, reject) {
+  console.log(4)
+  resolve()
+})
+p.then(function () {
+  console.log(5)
+})
+console.log(6)
