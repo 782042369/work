8246c45f7a5bc1a2a1028ce9de5fea96bd0bcdfd
yanghongxuan <782042369@qq.com>
2019-04-23 15:04:49 +0800

    react

--- index.ts
+++
@@ -1,7 +0,0 @@
-console.log('你好tsts')
-let str: string = 'index ts'
-
-function getdata() {
-	console.log('你好hello word')
-}
-getdata()
--- js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log('你好tsts');
-var str = 'index ts';
-function getdata() {
-    console.log('你好hello word');
-}
-getdata();
