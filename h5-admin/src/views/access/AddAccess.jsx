c54b31ea60fd6b779c8dd7cc0782125555feff8b
yanghongxuan <782042369@qq.com>
2019-05-21 10:23:01 +0800

    权限模块修改

--- h5-admin/src/views/access/AddAccess.jsx
+++ h5-admin/src/views/access/AddAccess.jsx
@@ -3,80 +3,84 @@
  * @LastEditors: 杨宏旋
  * @Description: 权限
  * @Date: 2019-05-05 15:48:39
- * @LastEditTime: 2019-05-17 12:20:11
+ * @LastEditTime: 2019-05-21 10:19:49
  */
-import React, { Component } from 'react';
-import { addaccess, editaccess, accesslist } from '../../api/access';
-import getUrlParam from '../../tool/getUrlParam';
-import { Form, Input, Button, message, Select } from 'antd';
-const Option = Select.Option;
+import React, { Component } from 'react'
+import { addaccess, editaccess, accesslist } from '../../api/access'
+import getUrlParam from '../../tool/getUrlParam'
+import { message } from 'antd'
+import BashForm from '../../components/Form'
 
-class WrappedRegistrationForm extends Component {
+class manger extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
 		this.state = {
 			title: '',
 			accessdata: [],
 			roledata: []
-		};
+		}
 	}
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.props.form.validateFieldsAndScroll((err, values) => {
-			if (!err) {
-				values.sort = Number(values.sort);
-				if (getUrlParam('id')) {
-					let arr = { id: getUrlParam('id') };
-					editaccess(Object.assign(values, arr))
-						.then((res) => {
-							console.log(res);
-							message.success(res.message);
-						})
-						.catch((err) => {
-							message.error(err.message);
-							console.log(err);
-						});
-				} else {
-					addaccess(values)
-						.then((res) => {
-							console.log(res);
-							message.success(res.message);
-						})
-						.catch((err) => {
-							console.log(err);
-							message.error(err.message);
-						});
-				}
+	handleSubmit = (values) => {
+		if (!err) {
+			values.sort = Number(values.sort)
+			if (getUrlParam('id')) {
+				let arr = { id: getUrlParam('id') }
+				editaccess(Object.assign(values, arr))
+					.then((res) => {
+						console.log(res)
+						message.success(res.message)
+					})
+					.catch((err) => {
+						message.error(err.message)
+						console.log(err)
+					})
+			} else {
+				addaccess(values)
+					.then((res) => {
+						console.log(res)
+						message.success(res.message)
+					})
+					.catch((err) => {
+						console.log(err)
+						message.error(err.message)
+					})
 			}
-		});
+		}
 	}
 	getlist() {
 		accesslist({ module_id: 0 })
 			.then((res) => {
 				if (res.status === 1) {
+					let list = [ { id: 0, name: '顶级模块' } ]
+					res.data.forEach((element) =>
+						list.push({
+							id: element._id,
+							name: element.module_name
+						})
+					)
+					console.log('list: ', list)
 					this.setState({
-						accessdata: res.data
-					});
-					console.log('this.state.accessdata: ', this.state.accessdata);
+						list
+					})
 				}
 			})
 			.catch((err) => {
-				console.log('err: ', err);
-			});
+				console.log('err: ', err)
+			})
 	}
 	componentDidMount() {
-		this.getlist();
+		this.getlist()
 		if (getUrlParam('id')) {
 			this.setState({
-				title: '修改'
-			});
+				h1title: '修改权限'
+			})
 			accesslist({
 				_id: getUrlParam('id')
 			})
 				.then((res) => {
-					let { action_name, description, module_id, module_name, sort, status, type, url } = res.data[0];
-					this.props.form.setFieldsValue({
+					let { action_name, description, module_id, module_name, sort, status, type, url } = res.data[0]
+					this.setState({
 						action_name,
 						description,
 						module_id,
@@ -85,134 +89,86 @@ class WrappedRegistrationForm extends Component {
 						status,
 						type,
 						url
-					});
+					})
 				})
 				.catch((err) => {
-					console.log('err: ', err);
-				});
+					console.log('err: ', err)
+				})
 		} else {
 			this.setState({
-				title: '增加'
-			});
+				h1title: '增加权限'
+			})
 		}
 	}
-	renderOptions = () => {
-		console.log('this.state.accessdata: ', this.state.accessdata);
-		return this.state.accessdata.map((element) => (
-			<Option key={element._id} value={element._id}>
-				{element.module_name}
-			</Option>
-		));
-	}
 	render() {
-		const { getFieldDecorator } = this.props.form;
-
-		const formItemLayout = {
-			labelCol: {
-				xs: { span: 24 },
-				sm: { span: 8 }
+		const formList = [
+			{
+				type: 'input',
+				lable: '模块名称',
+				setValue: this.state.module_name,
+				placeholder: '请输入',
+				field: 'module_name',
+				required: true,
+				message: 'Please input your module_name!'
 			},
-			wrapperCol: {
-				xs: { span: 24 },
-				sm: { span: 16 }
+			{
+				type: 'select',
+				lable: '节点名称',
+				setValue: this.state.type,
+				placeholder: '请输入',
+				field: 'type',
+				required: true,
+				message: 'Please input your type!',
+				list: [ { id: 1, name: '模块' }, { id: 2, name: '菜单' }, { id: 3, name: '操作' } ] // //1、模块 2、菜单 3、操作
+			},
+			{
+				type: 'input',
+				lable: '节点名称',
+				setValue: this.state.action_name,
+				placeholder: '请输入',
+				field: 'action_name',
+				required: false,
+				message: 'Please input your action_name!'
+			},
+			{
+				type: 'input',
+				lable: '操作地址',
+				setValue: this.state.url,
+				placeholder: '请输入',
+				field: 'url',
+				required: false,
+				message: 'Please input your url!'
+			},
+			{
+				type: 'select',
+				lable: '所属模块',
+				setValue: this.state.module_id,
+				placeholder: '请输入',
+				field: 'module_id',
+				required: true,
+				list: this.state.list, //0、顶级模块 其他菜单
+				message: 'Please input your module_id!'
+			},
+			{
+				type: 'input',
+				lable: '操作地址',
+				setValue: this.state.sort,
+				placeholder: '请输入',
+				field: 'sort',
+				required: true,
+				message: 'Please input your sort!'
+			},
+			{
+				type: 'input',
+				lable: '描述',
+				setValue: this.state.description,
+				placeholder: '请输入',
+				field: 'description',
+				required: true,
+				message: 'Please input your description!'
 			}
-		};
-		const tailFormItemLayout = {
-			wrapperCol: {
-				xs: {
-					span: 24,
-					offset: 0
-				},
-				sm: {
-					span: 16,
-					offset: 8
-				}
-			}
-		};
-
-		return (
-			<Form {...formItemLayout} onSubmit={this.handleSubmit}>
-				<h1>{this.state.title}权限</h1>
-				<Form.Item label="模块名称">
-					{getFieldDecorator('module_name', {
-						rules: [
-							{
-								required: true,
-								message: 'Please input your title!'
-							}
-						]
-					})(<Input placeholder="请输入" />)}
-				</Form.Item>
-				<Form.Item label="节点名称">
-					{getFieldDecorator('type', {
-						rules: [
-							{
-								required: true,
-								message: 'Please input your description!'
-							}
-						]
-					})(
-						//1、表示模块 2、表示菜单 3、操作
-						<Select onChange={this.handleChange}>
-							<Option key={1} value={1}>
-								模块
-							</Option>
-							<Option key={2} value={2}>
-								菜单
-							</Option>
-							<Option key={3} value={3}>
-								操作
-							</Option>
-						</Select>
-					)}
-				</Form.Item>
-				<Form.Item label="操作名称">{getFieldDecorator('action_name')(<Input placeholder="请输入" />)}</Form.Item>
-				<Form.Item label="操作地址">{getFieldDecorator('url')(<Input placeholder="请输入" />)}</Form.Item>
-				<Form.Item label="所属模块">
-					{getFieldDecorator('module_id', {
-						rules: [
-							{
-								required: true,
-								message: 'Please input your description!'
-							}
-						]
-					})(
-						<Select onChange={this.handleChange}>
-							<Option key={0} value={0}>
-								顶级模块
-							</Option>
-							{this.renderOptions()}
-						</Select>
-					)}
-				</Form.Item>
-				<Form.Item label="排序">
-					{getFieldDecorator('sort', {
-						rules: [
-							{
-								required: true,
-								message: 'Please input your description!'
-							}
-						]
-					})(<Input placeholder="请输入" />)}
-				</Form.Item>
-				<Form.Item label="描述">
-					{getFieldDecorator('description', {
-						rules: [
-							{
-								required: true,
-								message: 'Please input your description!'
-							}
-						]
-					})(<Input placeholder="请输入" />)}
-				</Form.Item>
-				<Form.Item {...tailFormItemLayout}>
-					<Button type="primary" htmlType="submit">
-						提交
-					</Button>
-				</Form.Item>
-			</Form>
-		);
+		]
+		return <BashForm formList={formList} h1title={this.state.h1title} formSubmit={this.handleSubmit} />
 	}
 }
-const manger = Form.create({ name: 'register' })(WrappedRegistrationForm);
-export default manger;
+export default manger
