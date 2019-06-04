a07369773d403776181e19f0d69fb5700beb1322
yanghongxuan <782042369@qq.com>
2019-06-04 10:12:03 +0800

    修改

--- react-antd-ts/config-overrides.js
+++ react-antd-ts/config-overrides.js
@@ -1,11 +1,43 @@
 const {
   override,
-  fixBabelImports
+  fixBabelImports,
 } = require('customize-cra');
+const {
+  BundleAnalyzerPlugin
+} = require("webpack-bundle-analyzer");
+const addCustomize = () => config => {
+  if (process.env.NODE_ENV === 'production') {
+    config.devtool = false; //去掉map文件
+    if (config.plugins) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin()
+      );
+    }
+    const splitChunksConfig = config.optimization.splitChunks;
+    Object.assign(splitChunksConfig, {
+      chunks: 'all',
+      cacheGroups: {
+        vendors: {
+          test: /node_modules/,
+          name: 'vendors',
+          priority: -10,
+        },
+        common: {
+          name: 'common',
+          minChunks: 2,
+          minSize: 30000,
+          chunks: 'all'
+        }
+      }
+    })
+  }
+  return config;
+}
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
   }),
+  addCustomize()
 );
