df45e46137b40328d8a10fdaa5aa6dbd50c637e1
yanghongxuan <yanghongxuan@tyrise.com>
2020-09-14 16:22:49 +0800

    [update]添加 demo

---
+++ 一维数组的动态和.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: 杨宏旋
+ * @Date: 2020-09-14 15:52:11
+ * @LastEditors: 杨宏旋
+ * @LastEditTime: 2020-09-14 16:22:37
+ * @Description:
+ */
+/**
+ * 一维数组的动态和
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const runningSum = function (nums) {
+  /**
+   * 执行用时：
+    84 ms
+    , 在所有 JavaScript 提交中击败了
+    59.33%
+    的用户
+    内存消耗：
+    39.1 MB
+    , 在所有 JavaScript 提交中击败了
+    12.54%
+    的用户
+   */
+  // const arr = []
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (arr.length < 1) {
+  //     arr.push(nums[0])
+  //   } else {
+  //     arr.push(nums[i] + arr[i - 1])
+  //   }
+  // }
+  // return arr
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = nums[i] + nums[i - 1]
+  }
+  return nums
+}
+const nums = [1, 2, 3, 4]
+// [1,3,6,10]
+console.log('runningSum(nums): ', runningSum(nums))
---
+++ 无重复字符的最长子串.js
@@ -0,0 +1,50 @@
+/*
+ * @Author: 杨宏旋
+ * @Date: 2020-01-18 10:39:57
+ * @LastEditors: 杨宏旋
+ * @LastEditTime: 2020-09-14 16:22:17
+ * @Description: 无重复字符的最长子串
+ */
+/* 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+    示例 1:
+    输入: "abcabcbb"
+    输出: 3
+    解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+    示例 2:
+    输入: "bbbbb"
+    输出: 1
+    解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+
+    示例 3:
+    输入: "pwwkew"
+    输出: 3
+    解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+    请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function (s) {
+  let i = 0,
+    k = 0,
+    max = 0,
+    str = ''
+  while (i < s.length) {
+    if (str == '') {
+      k = i
+    }
+    if (str.indexOf(s[i]) == -1) {
+      str += s[i]
+      i++
+    } else {
+      max = Math.max(max, str.length)
+      i = k + str.indexOf(s[i]) + 1
+      str = ''
+    }
+  }
+  return Math.max(max, str.length)
+}
+const arr = 'abedasdasdadasfesdgerhrtejhhjyrjetgcabcbb'
+lengthOfLongestSubstring(arr)
